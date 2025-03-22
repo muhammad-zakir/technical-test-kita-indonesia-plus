c4ac2d210703a08772f25a8dc44431acbfca7216
Muhammad Zakir <hi@zakir.id>
2025-03-22 07:42:11 +0700

    Add endpoints for fetching users

--- library/urls.py
+++ library/urls.py
@@ -2,9 +2,11 @@ from django.urls import include, path
 from rest_framework import routers
 
 from books import views as books_views
+from library import views as library_views
 
 router = routers.DefaultRouter()
 router.register(r'books', books_views.BookViewSet)
+router.register(r'users', library_views.UserViewSet)
 # router.register(r'users', views.UserViewSet)
 # router.register(r'groups', views.GroupViewSet)
 
